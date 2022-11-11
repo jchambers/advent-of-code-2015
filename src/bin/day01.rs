132a820e3d5d8aaa6ccb21fa04d3d988318ea2ee
Jon Chambers <jon.chambers@gmail.com>
2022-11-10 23:52:41 -0500

    Add a solution for day 1, part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -12,6 +12,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         file.read_to_string(&mut directions)?;
 
         println!("Destination floor: {}", get_floor(&directions));
+        println!("First position leading to basement: {}", find_first_basement_position(&directions));
 
         Ok(())
     } else {
@@ -29,9 +30,27 @@ fn get_floor(directions: &str) -> i32 {
         .sum()
 }
 
+fn find_first_basement_position(directions: &str) -> usize {
+    let mut current_floor = 0;
+
+    for (i, c) in directions.chars().enumerate() {
+        current_floor += match c {
+            '(' => 1,
+            ')' => -1,
+            _ => 0
+        };
+
+        if current_floor < 0 {
+            return i + 1;
+        }
+    }
+
+    panic!()
+}
+
 #[cfg(test)]
 mod test {
-    use crate::get_floor;
+    use crate::{find_first_basement_position, get_floor};
 
     #[test]
     fn test_get_floor() {
@@ -44,5 +63,11 @@ mod test {
         assert_eq!(-1, get_floor("))("));
         assert_eq!(-3, get_floor(")))"));
         assert_eq!(-3, get_floor(")())())"));
+    }
+
+    #[test]
+    fn test_find_first_basement_position() {
+        assert_eq!(1, find_first_basement_position(")"));
+        assert_eq!(5, find_first_basement_position("()())"));
     }
 }
